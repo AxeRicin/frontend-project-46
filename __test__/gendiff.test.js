6c99b11b8b2aa3fcbee211d6f1a4a36e7e82305b
AxeRicin <AxeRicin@yandex.ru>
2023-07-11 05:57:04 +0300

    Add tests for Plain Format

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -11,17 +11,19 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const parseFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 const fixtureFlatFileNames = [
-  ['file1.json', 'file2.json', 'expectedResult.txt'],
-  ['file1.yaml', 'file2.yaml', 'expectedResult.txt'],
-  ['file1.yml', 'file2.yml', 'expectedResult.txt'],
+  ['stylish', 'file1.json', 'file2.json', 'expectedResultStylish.txt'],
+  ['stylish', 'file1.yaml', 'file2.yaml', 'expectedResultStylish.txt'],
+  ['stylish', 'file1.yml', 'file2.yml', 'expectedResultStylish.txt'],
+  ['plain', 'file1.json', 'file2.json', 'expectedResultPlain.txt'],
+  ['plain', 'file1.yaml', 'file2.yaml', 'expectedResultPlain.txt'],
 ];
 
 describe('genDiff', () => {
-  test.each(fixtureFlatFileNames)('should work with nested structures', (file1, file2, result) => {
+  test.each(fixtureFlatFileNames)('should work with nested structures %p', (formatter, file1, file2, result) => {
     const filePath1 = getFixturePath(file1);
     const filePath2 = getFixturePath(file2);
     const expected = parseFixture(result);
-    expect(genDiff(filePath1, filePath2)).toEqual(expected);
+    expect(genDiff(filePath1, filePath2, formatter)).toEqual(expected);
   });
   /* test('should throw an error on unknown file format', () => {
     const filePath1 = getFixturePath('file1.json');
