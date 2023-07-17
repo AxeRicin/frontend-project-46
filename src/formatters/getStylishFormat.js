eacdf78fb5573de691852aebd0cb1ed03366c7a7
AxeRicin <AxeRicin@yandex.ru>
2023-07-17 15:49:26 +0300

    Update getStylishFormat.js

--- src/formatters/getStylishFormat.js
+++ src/formatters/getStylishFormat.js
@@ -1,41 +1,43 @@
 import _ from 'lodash';
 
-const getIndented = (depth, shiftLeft = 2) => {
-  const indent = ' ';
-  const numberOfIndents = 4;
-  return indent.repeat(depth * numberOfIndents - shiftLeft);
-};
+const indent = ' ';
+const numberOfIndents = 4;
+const shiftLeft = 2;
+
+const getIndentedShift2 = (depth) => indent.repeat(depth * numberOfIndents - shiftLeft);
+
+const getIndentedShift0 = (depth) => indent.repeat(depth * numberOfIndents);
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return String(data);
   }
   const entries = Object.entries(data);
-  const result = entries.map(([key, value]) => `${getIndented(depth, 0)}${key}: ${stringify(value, depth + 1)}`, '');
-  return `{\n${result.join('\n')}\n${getIndented(depth, 4)}}`;
+  const result = entries.map(([key, value]) => `${getIndentedShift0(depth)}${key}: ${stringify(value, depth + 1)}`, '');
+  return `{\n${result.join('\n')}\n${getIndentedShift0(depth - 1)}}`;
 };
 
 const getStylishFormat = (diff, depth = 1) => {
   const lines = diff.map((node) => {
     switch (node.type) {
       case 'nested':
-        return `${getIndented(depth, 0)}${node.key}: ${getStylishFormat(node.value, depth + 1)}`;
+        return `${getIndentedShift0(depth)}${node.key}: ${getStylishFormat(node.value, depth + 1)}`;
       case 'deleted':
-        return `${getIndented(depth)}- ${node.key}: ${stringify(node.value, depth + 1)}`;
+        return `${getIndentedShift2(depth)}- ${node.key}: ${stringify(node.value, depth + 1)}`;
       case 'added':
-        return `${getIndented(depth)}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
+        return `${getIndentedShift2(depth)}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
       case 'changed': {
-        const del = `${getIndented(depth)}- ${node.key}: ${stringify(node.oldValue, depth + 1)}`;
-        const add = `\n${getIndented(depth)}+ ${node.key}: ${stringify(node.newValue, depth + 1)}`;
+        const del = `${getIndentedShift2(depth)}- ${node.key}: ${stringify(node.oldValue, depth + 1)}`;
+        const add = `\n${getIndentedShift2(depth)}+ ${node.key}: ${stringify(node.newValue, depth + 1)}`;
         return del + add;
       }
       case 'unchanged':
-        return `${getIndented(depth, 0)}${node.key}: ${stringify(node.value, depth + 1)}`;
+        return `${getIndentedShift0(depth)}${node.key}: ${stringify(node.value, depth + 1)}`;
       default:
         throw new Error(`Неверный тип узла: ${node.type}`);
     }
   });
-  return `{\n${lines.join('\n')}\n${getIndented(depth - 1, 0)}}`;
+  return `{\n${lines.join('\n')}\n${getIndentedShift0(depth - 1)}}`;
 };
 
 export default getStylishFormat;
